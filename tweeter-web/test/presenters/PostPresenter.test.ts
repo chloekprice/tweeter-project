aa01f6c251cf6947f1c7c1226f34deaa4033cfc9
Chloe Brown <144487076+chloekprice@users.noreply.github.com>
2025-10-21 00:08:00 -0600

    positive test case for displaying posting status info message

--- tweeter-web/test/presenters/PostPresenter.test.ts
+++ tweeter-web/test/presenters/PostPresenter.test.ts
@@ -1,7 +1,7 @@
 import { AuthToken } from "tweeter-shared";
 import PostService from "../../src/models/PostService";
 import PostPresenter, { PostView } from "../../src/presenters/PostPresenter"
-import { anything, instance, mock, spy, when } from "@typestrong/ts-mockito";
+import { anything, instance, mock, spy, verify, when } from "@typestrong/ts-mockito";
 
 describe("PostPresenter", () => {
     let mockPostPresenterView: PostView;
@@ -14,14 +14,19 @@ describe("PostPresenter", () => {
         mockPostPresenterView = mock<PostView>();
         mockService = mock<PostService>();
 
-        const mockPostPresenterViewInstance = instance(mockPostPresenterView);
-        const mockServiceInstance = instance(mockService);
+        let mockPostPresenterViewInstance = instance(mockPostPresenterView);
+        let mockServiceInstance = instance(mockService);
 
         let postPresenter = new PostPresenter(mockPostPresenterViewInstance);
-        let postPresenterSpy = spy(postPresenter);
+        const postPresenterSpy = spy(postPresenter);
         postPresenterSpyInstance = instance(postPresenterSpy);
 
-        when(postPresenterSpyInstance.postService).thenReturn(mockServiceInstance);
+        when(postPresenterSpy.postService).thenReturn(mockServiceInstance);
         when(mockPostPresenterView.displayInfoMsg(anything(), 0)).thenReturn("messageId");
+    })
+
+    it("tells the view to display a posting status message", async () => {
+        await postPresenterSpyInstance.submitPost(anything(), anything(), authToken);
+        verify(mockPostPresenterView.displayInfoMsg("Posting status...", 0)).once();
     })
 })
