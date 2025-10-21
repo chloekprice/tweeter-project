daaecb1b7772f30eb477ecd0ac753ac832356c60
Chloe Brown <144487076+chloekprice@users.noreply.github.com>
2025-10-21 16:21:38 -0600

    create test to check sign in button disabled when fields empty

--- tweeter-web/test/views/Authentication/Login/Login.test.tsx
+++ tweeter-web/test/views/Authentication/Login/Login.test.tsx
@@ -1,4 +1,36 @@
+import { render, screen } from "@testing-library/react"
+import Login from "../../../../src/views/authentication/login/Login"
+import { MemoryRouter } from "react-router-dom";
+import { userEvent } from "@testing-library/user-event";
+import "@testing-library/jest-dom"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+library.add(fab);
 
 describe("Login View", () => {
-    
+
+    it("starts with the sign in button disabled", async () => {
+        const { signInButton } = renderLoginAndGetElements("/");
+        expect(signInButton).toBeDisabled();
+    })
 })
+
+function renderLogin(originalUrl: string) {
+    return render(
+        <MemoryRouter>
+            <Login originalUrl={originalUrl} />
+        </MemoryRouter>
+    );
+}
+
+function renderLoginAndGetElements(originalUrl: string) {
+    const user = userEvent.setup();
+    renderLogin(originalUrl);
+
+    const signInButton = screen.getByRole("button", {name: /Sign in/});
+    const aliasField = screen.getByLabelText("alias");
+    const passwordField = screen.getByLabelText("password");
+
+    return { user, signInButton, aliasField, passwordField };
+}
