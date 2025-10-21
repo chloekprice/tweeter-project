4218e6fd06bb4e47f56196ecffb5694daeaab2e2
Chloe Brown <144487076+chloekprice@users.noreply.github.com>
2025-10-21 17:45:10 -0600

    create test for submitting post

--- tweeter-web/test/views/postStatus/PostStatus.test.tsx
+++ tweeter-web/test/views/postStatus/PostStatus.test.tsx
@@ -3,14 +3,10 @@ import PostStatus from "../../../src/views/postStatus/PostStatus"
 import { MemoryRouter } from "react-router-dom";
 import { userEvent } from "@testing-library/user-event";
 import "@testing-library/jest-dom"
-import { fab } from "@fortawesome/free-brands-svg-icons"
-import { library } from "@fortawesome/fontawesome-svg-core";
 import PostPresenter from "../../../src/presenters/PostPresenter";
 import { deepEqual, instance, mock, verify } from "@typestrong/ts-mockito";
 import { useUserInfo } from "../../../src/views/userInfo/UserInfoHooks";
 import { AuthToken, User } from "tweeter-shared";
-
-library.add(fab);
 
 
 jest.mock("../../../src/views/userInfo/UserInfoHooks", () => ({
