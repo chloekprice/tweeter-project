d319923a4ab20649cfe2514f554a435913f0bc73
Chloe Brown <144487076+chloekprice@users.noreply.github.com>
2025-11-10 20:04:21 -0700

    create lambda for retrieving followers and refactor shared code into a helper function

---
+++ tweeter-server/src/utils/GetUserItemsHelper.ts
@@ -0,0 +1,19 @@
+import { PagedUserItemRequest, PagedUserItemResponse, UserDto } from "tweeter-shared";
+
+export const helper = async (request: PagedUserItemRequest, loadFunction: () => Promise<[any, any]>): Promise<PagedUserItemResponse> => {
+    if (!request.token || !request.userAlias || !request.pageSize) {
+        throw new Error("Bad Request: the request does not include all required parameters");
+    }
+    if (typeof request.token !== "string") {
+        throw new Error("Unauthorized: there are insufficient permissions to perform this action")
+    }
+
+    const [items, hasMore] = await loadFunction();
+
+    return {
+        success: true,
+        message: null, 
+        items: items,
+        hasMore: hasMore
+    }
+}
